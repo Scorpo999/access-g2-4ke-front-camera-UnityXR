6a5fe58f6d971b0de06b83a284472dd26913fbe2
Pico <46362299+picoxr@users.noreply.github.com>
2021-02-24 11:16:14 +0800

    20210224

---
+++ Assets/InputManger.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class InputManger : MonoBehaviour
+{
+    public RectTransform rawImage;
+    public RectTransform Image;
+    public Vector2 RawImageVector;
+    public Vector2 ImageVector;
+
+    //zoom in/out scale
+    private float ScaleHeight;
+    private float ScaleWidth;
+    private Vector2 AddScale;
+    private int IsClick = 0;
+
+
+    private const int KEY_COUNTER = 10;
+
+    public Text text;
+
+    private void Awake()
+    {
+        RawImageVector = rawImage.sizeDelta;
+        ImageVector = Image.sizeDelta;
+        ScaleHeight = Image.rect.height / 10;
+        ScaleWidth = Image.rect.width / 10;
+    }
+
+    private void Update()
+    {
+        PicoInput();
+    }
+    public void PicoInput()
+    {
+
+        // Press Back button on HMD 
+        if (Input.GetKeyDown(KeyCode.JoystickButton0))
+        {
+            if (IsClick == KEY_COUNTER)
+            {
+                return;
+            }
+            if (IsClick < KEY_COUNTER)
+            {
+                IsClick += 1;
+                AddScale.Set(RawImageVector.x + ScaleWidth, RawImageVector.y + ScaleHeight);
+                rawImage.sizeDelta = AddScale;
+                RawImageVector = rawImage.sizeDelta;
+            }
+
+        }
+
+        
+
+        // Press Confirm button on HMD 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (IsClick == 0)
+            {
+                return;
+            }
+            if (IsClick > 0)
+            {
+                IsClick -= 1;
+                AddScale.Set(RawImageVector.x - ScaleWidth, RawImageVector.y - ScaleHeight);
+                rawImage.sizeDelta = AddScale;
+                RawImageVector = rawImage.sizeDelta;
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # access-g2-4ke-front-camera-UnityXR
+
+## Unity Versions：
+- 2019.4.16f1 and later
+
+## Description：
+
+- This demo shows how to access Pico G2 4KE front camera in Unity project. Please refer to the codes in **GetPhoneCameraImageDemo.cs** script.
+
+## Usage：
+- Scene： Assets -> DemoScene -> GetPhoneCameraImageDemo
+Press the keys on HMD to zoom in or out.
+App key: Zoom in
+Confirm key: Zoom out
+
+
+## Note: 
+- Pico_UnityXR_SDK is not in the Demo, so the SDK needs to be imported separately after downloading the Demo. https://developer.pico-interactive.com/sdk/index?id=8
